f0d0f2b744318aa40e4b05bb9d7ae38acb832e74
AndreeaSB <andreea.sbadea@gmail.com>
2024-05-25 20:17:37 +0300

    set up server and db connection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const FoodModel = require('./models/Food')
 
 const app = express()
-const PORT = 3000
+const PORT = 3000;
+const MONGO_URI = 'mongodb+srv://AndreeaSB:testing123@cluster0.hrc7roo.mongodb.net/CrudApp';
 
-app.use(express.json)
+ app.use(express.json())
 
-mongoose.connect('')
+ mongoose.Promise = global.Promise;
+ mongoose.connect(MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true}).then(() => {
+     console.log("Connected to MongoDB successfully :)");
+ }).catch((e) => {
+     console.log("Error while attemting to connect to MongoDB");
+     console.log(e);
+ });
+
+ app.get('/', (req,res) => {
+    console.log("hello lume!")
+    res.send("Hello lume!")
+})
+
+app.get('/getsmth', async (req, res) => {
+    console.log(`I'm in`)
+    const food = new FoodModel({
+        foodName: 'apple',
+        daysSinceIAte: 3
+    })
+    try {
+        await food.save()
+    } catch(err) {
+        console.log(err)
+    }
+}) 
 
 app.listen(PORT, () => {
-    console.log('Server is running :)')
+    console.log(`Server is running on port ${PORT} :)`)
 })
